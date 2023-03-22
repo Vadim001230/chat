033b1e174525c6ae9ebc6ce83182197d616096c9
Vadim Shershen <vadim.shershen.97@mail.ru>
2023-03-22 12:01:04 +0300

    feat: add message API and redux store

---
+++ client/src/redux/store.ts
@@ -0,0 +1,11 @@
+import { configureStore } from '@reduxjs/toolkit';
+import { messageApi } from './messageApi';
+
+const store = configureStore({
+  reducer: {
+    [messageApi.reducerPath]: messageApi.reducer,
+  },
+  middleware: (getDefaultMiddlware) =>
+    getDefaultMiddlware().concat(messageApi.middleware),
+});
+export default store;
